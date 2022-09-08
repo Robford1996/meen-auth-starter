f94d173bda455caaef212dfdf79f4a95ca446f9e
Robert Ford <robert.ford1996@gmail.com>
2022-09-08 14:05:53 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose")
+
+const PORT = process.env.PORT
+
+//Database Configuration
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+//Database Connection Error/Success
+
+const db = mongoose.connection;
+db.on("error", (err)=> console.log(err.message + "is mongoose not running?"))
+db.on("connected", ()=> console.log("mongo connected"))
+db.on("disconnected", ()=> console.log("mongo disconnected"))
+
+//Middleware
+//Body Parser middleware: give us access to req.body
+app.use(express.urlencoded({extended: true}))
+
+//Server Test
+// app.get("/", (req, res)=>{
+//     res.send("Server is working")
+// })
+
+//Listener
+app.listen(PORT, ()=>{
+    console.log(`server is listening on port ${PORT}`)
+})
